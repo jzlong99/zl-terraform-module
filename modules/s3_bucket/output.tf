5e0641474fd03629e3bda5e9548e2e5c155d476a
Jin Zhenglong <jzlong99@gmail.com>
2024-11-09 23:17:13 +0800

    update output for s3 module

--- modules/s3_bucket/output.tf
+++ modules/s3_bucket/output.tf
@@ -2,32 +2,32 @@
 
 output "name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.s3_bucket_object.id
+  value       = try(aws_s3_bucket.s3_bucket_object.id, null)
 }
 
 output "arn" {
   description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.s3_bucket_object.arn
+  value       = try(aws_s3_bucket.s3_bucket_object.arn, null)
 }
 
 output "region" {
   description = "The AWS region where the S3 bucket is located"
-  value       = aws_s3_bucket.s3_bucket_object.region
+  value       = try(aws_s3_bucket.s3_bucket_object.region, null)
 }
 
 output "bucket_domain_name" {
   description = "The bucket domain name of the S3 bucket"
-  value       = aws_s3_bucket.s3_bucket_object.bucket_domain_name
+  value       = try(aws_s3_bucket.s3_bucket_object.bucket_domain_name, null)
 }
 
 output "bucket_regional_domain_name" {
   description = "The bucket regional domain name of the S3 bucket"
-  value       = aws_s3_bucket.s3_bucket_object.bucket_regional_domain_name
+  value       = try(aws_s3_bucket.s3_bucket_object.bucket_regional_domain_name, null)
 }
 
 output "tags" {
   description = "The tags assigned to the S3 bucket"
-  value       = aws_s3_bucket.s3_bucket_object.tags
+  value       = try(aws_s3_bucket.s3_bucket_object.tags, null)
 
   precondition {
     condition     = aws_s3_bucket.s3_bucket_object.tags
@@ -37,5 +37,5 @@ output "tags" {
 
 output "id" {
   description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket_object_lock_configuration.s3_bucket_object_lock_object.id
+  value       = try(aws_s3_bucket_object_lock_configuration.s3_bucket_object_lock_object.id, null)
 }
