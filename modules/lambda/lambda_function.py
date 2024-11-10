2533d4f18b3413398b3882785f0741f9ca5e6196
Jin Zhenglong <jzlong99@gmail.com>
2024-11-10 17:07:17 +0800

    added lambda module

---
+++ modules/lambda/lambda_function.py
@@ -0,0 +1,14 @@
+# import json
+
+# def lambda_handler(event, context):
+#     # TODO: Implement your logic here
+
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
+
+
+def handler(event, context):
+    print("This is a placeholder Lambda function")
+    return
